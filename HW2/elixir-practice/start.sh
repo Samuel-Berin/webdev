05405c3af679af8bc11fc68e22148fb21163b624
Samuel-Berin <berin.s@husky.neu.edu>
2018-09-20 16:06:36 -0400

    Adding start and nginx file

---
+++ HW2/elixir-practice/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+export MIX_ENV=prod
+export PORT=4790
+
+echo "Stopping old copy of app, if any..."
+
+_build/prod/rel/practice/bin/practice stop || true
+
+echo "Starting app..."
+
+_build/prod/rel/practice/bin/practice Start
+
+
+# TODO: Add a cron rule or systemd service file
+#       to start your app on system boot.
