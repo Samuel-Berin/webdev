55e29fd19943934fe4420599ff7b463f00458f02
Samuel-Berin <berin.s@husky.neu.edu>
2018-09-20 22:03:19 -0400

    Added inspect

--- HW2/elixir-practice/lib/practice_web/controllers/page_controller.ex
+++ HW2/elixir-practice/lib/practice_web/controllers/page_controller.ex
@@ -18,8 +18,7 @@ defmodule PracticeWeb.PageController do
 
   def factor(conn, %{"x" => x}) do
     {x, _} = Integer.parse(x)
-    y = Practice.factor(x)
-    y = binary.bin_to_list(y)
+    y = inspect(Practice.factor(x))
     render conn, "factor.html", x: x, y: y
   end
 
